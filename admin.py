a86b4848ddbf4cdeb650c4820c52bd2b82af2673
Shobhit Gupta <90123600+shobhu92@users.noreply.github.com>
2022-02-21 10:05:50 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+admin.site.register(Book)
+admin.site.register(Student)
+admin.site.register(Borrower)
+admin.site.register(Genre)
+admin.site.register(Language)
---
+++ forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from .models import *
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
+class BorrowForm(forms.ModelForm):
+    class Meta:
+        model = Borrower
+        exclude = ['issue_date', 'return_date']
+
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = '__all__'
